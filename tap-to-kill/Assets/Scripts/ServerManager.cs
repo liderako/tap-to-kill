d9f3f5fdcb38174b9e5672aa697e02a61e062a71
liderako <trotillostudio@gmail.com>
2019-05-05 20:48:11 +0300

    add connected to server photon

---
+++ tap-to-kill/Assets/Scripts/ServerManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ServerManager : Photon.PunBehaviour
+{
+    [SerializeField]private bool isConnected;
+    [SerializeField]private string scene;
+
+    private void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings("");
+    }
+
+    public void OnConnectedToMaster()
+    {
+        isConnected = true;
+    }
+
+    public void Update()
+    {
+        if (isConnected)
+        {
+            SceneManager.LoadScene(scene, LoadSceneMode.Single);
+        }
+    }
+}
